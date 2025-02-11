8f7ad6608f05a31bdba669a2170a4a9dd2fa26ee
skgmsj <sebastiankarstrom0@gmail.com>
2025-02-11 12:52:47 +0100

    Developed the recipe creator (#4)

--- app/controllers/RecipesController.py
+++ app/controllers/RecipesController.py
@@ -1,6 +1,21 @@
-class RecipesController:
+from typing import List, Tuple
 
-    # Create recipe
+from app.models import session_scope, Recipe, Ingredient, RecipeIngredient
+
+
+class RecipesController:
+    def create(self, ingredients: List[Tuple[int, int]], **kwargs) -> Recipe:
+        with session_scope() as session:
+            recipe = Recipe(**kwargs)
+            recipe_ingredients = []
+            for ingredient in ingredients:
+                db_ingredient = session.query(Ingredient).filter_by(id=ingredient[0]).first()
+                if db_ingredient is None: continue
+                recipe_ingredient = RecipeIngredient(recipe=recipe, ingredient=db_ingredient, quantity=ingredient[1])
+                recipe_ingredients.append(recipe_ingredient)
+            session.add(recipe)
+            session.add_all(recipe_ingredients)
+            return recipe
 
     # Get all recipes (can be filtered by name)
 
