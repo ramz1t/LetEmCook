a5dbbff74e58d554759630359a8d768162f2cb77
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-02-16 13:08:24 +0100

    Outlined NotesController

---
+++ app/controllers/NotesController.py
@@ -0,0 +1,12 @@
+class NotesController:
+    def create(self, **kwargs):
+        pass
+
+    def list(self, search: str = str()):
+        pass
+
+    def update(self, **kwargs):
+        pass
+
+    def delete(self, id: int):
+        pass
