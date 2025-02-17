640693169989710bede549ec72798d6101f479ff
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-02-17 18:34:21 +0100

    get_recipe_count_label()

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,7 @@
-# UTILS IF NEEDED
+def get_recipes_count_label(count: int) -> str:
+    if count > 1:
+        return f"{count} ingredients"
+    elif count == 1:
+        return f"1 ingredient"
+    else:
+        return "No ingredients"
