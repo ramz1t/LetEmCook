35a5cbaffafb338930f04382fc91363ff37519d6
MAVA <wrt545@MacBook-Air-de-wrt545.local>
2025-02-18 10:19:48 +0100

    create fixed

--- app/controllers/NotesController.py
+++ app/controllers/NotesController.py
@@ -2,11 +2,18 @@ from app.models import session_scope, Note
 
 class NotesController:
     def create(self, **kwargs) -> dict:
+        try:
+            if not kwargs.get("title") or not kwargs.get("text"):
+                return {"error": "title or text missing"}
+
             with session_scope() as session:
                 note = Note(**kwargs)
                 session.add(note)
                 session.commit()
                 return note.to_dict()
+
+        except Exception as e:
+            return {"Error": f"Something went wrong: {str(e)}"}
 
     def list(self, search: str = str()):
         pass
