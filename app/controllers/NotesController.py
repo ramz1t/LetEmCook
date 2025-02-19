b9df1f7d6a512d63e15ce41b1cfdfdca1fa55b85
Valentina Goncalves <163341711+mavagoncalves@users.noreply.github.com>
2025-02-19 15:38:35 +0100

    Create notes (#10)
    * create notes
    
    * create fixed
    
    * create raise exception
    
    ---------
    
    Co-authored-by: MAVA <wrt545@MacBook-Air-de-wrt545.local>

--- app/controllers/NotesController.py
+++ app/controllers/NotesController.py
@@ -1,6 +1,19 @@
+from app.models import session_scope, Note
+
 class NotesController:
-    def create(self, **kwargs):
-        pass
+    def create(self, **kwargs) -> dict:
+        try:
+            if not kwargs.get("title") or not kwargs.get("text"):
+                raise ValueError("Error: title or text missing")
+
+            with session_scope() as session:
+                note = Note(**kwargs)
+                session.add(note)
+                session.commit()
+                return note.to_dict()
+
+        except Exception as e:
+            raise Exception(f"Something went wrong: {str(e)}")
 
     def list(self, search: str = str()):
         pass
