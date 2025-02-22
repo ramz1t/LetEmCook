2b92abeed35f5d4eba6459be747e37378963c465
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-02-22 12:18:50 +0100

    clear_layout, style_h1, style_h2

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,7 @@
 # --- For frontend ---
+from PyQt5.QtWidgets import QLayout, QLabel
+
+
 def get_recipes_count_label(count: int) -> str:
     if count > 1:
         return f"{count} ingredients"
@@ -7,5 +10,23 @@ def get_recipes_count_label(count: int) -> str:
     else:
         return "No ingredients"
 
+def clear_layout(layout: QLayout):
+    while layout.count():
+        item = layout.takeAt(0)
+        widget = item.widget()
+        if widget is not None:
+            widget.deleteLater()
 
-# --- For backend ---
+def style_h1(label: QLabel):
+    label.setStyleSheet("""
+        font-weight: bold;
+        font-size: 18px;
+    """)
+
+def style_h2(label: QLabel):
+    label.setStyleSheet("""
+        font-weight: bold;
+        font-size: 14px;
+    """)
+
+        # --- For backend ---
