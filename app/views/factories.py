f0933f727c2c0399ed27d1eee3a9203d8821e652
Roberto Rodriguez Espejo <rrodriguezesp@yahoo.es>
2025-03-17 08:50:21 +0100

    Provisional format

--- app/views/factories.py
+++ app/views/factories.py
@@ -15,30 +15,32 @@ from app.views.Settings.SettingsView import SettingsView
 
 def home_page_factory(**kwargs):
     introduction = QLabel('''
-    <br><br><br>
+    <br><br>
     <p style="text-align: center; font-size: 30px;">
     <b>Welcome to 
     </p>
     <p style="text-align: center; font-size: 40px;">
     <b><span style="font-size: 40px;">Let'EmCook</span>!</b>
     </p>
-    <br><br>
-    <p style="font-size: 20px;">The goal of this app is to help you manage your diet so you can keep a better track of the nutrients and <br> 
-    calories you eat to make you feel better.</p>
+    <br>
+    <p style="font-size: 18px;">The goal of this app is to help you manage your diet so you can keep a better track of the nutrients and calories you <br> eat to make you feel better.</p>
 
     <hr>
+    <p style="font-size: 15px;">
     <b>|RECIPES|</b>: Here you can create, edit and delete your recipes.<br>
     <b>|NOTES|</b>: Create any notes you need, talk about your recipes, how they make you feel, or experiences you have.<br>
     <b>|PLANNER|</b>: Get meal suggestions based on your activity to reach a recommended calorie level.<br>
     <b>|SETTINGS|</b>: Do not forget to update your body metrics and age as time goes by!<br>
+    </p>
     <hr>
 
+    <p style="font-size: 15px;">
     <b>Remember:</b><br>
-    - Having enough energy helps you stay on track with your routine. Carbohydrates offer a stable energy source while sugars may cause a fluctuation in energy levels.<br>
+    - Having enough energy helps you stay on track with your routine. Carbohydrates offer a stable energy source while sugars may cause a <br> &nbsp; fluctuation in energy levels.<br>
     - Foods rich in tryptophan (turkey, bananas, dairy) help produce serotonin, the "happiness hormone."<br>
-    - <b>Stress and Anxiety Reduction:</b> Omega-3 fatty acids (fish, nuts) and antioxidants (fruits, vegetables) can help lower stress and improve emotional well-being.<br>
-    - Tracking your mood and your feelings with recipes will help you achieve the diet you need to confront everyday plans. Looking back at your notes can give you valuable insights to improve your diet and well-being.<br>
-
+    - Stress and Anxiety Reduction: Omega-3 fatty acids (fish, nuts) and antioxidants (fruits, vegetables) can help lower stress and improve <br> &nbsp; emotional well-being.<br>
+    - Tracking your mood and your feelings with recipes will help you achieve the diet you need to confront everyday plans. Looking back at your <br> &nbsp; notes can give you valuable insights to improve your diet and well-being.<br>
+    </p>
     <hr>
     <p><b>This app has been created by:</b></p>
     <ul>
@@ -48,6 +50,7 @@ def home_page_factory(**kwargs):
         <li><a href="https://github.com/mavagoncalves">Maria Valentina Gonçalves Rojas</a></li>
         <li><a href="https://github.com/robrodres">Roberto Rodríguez Espejo</a></li>
     </ul>
+    <br>
     ''')
 
     # This makes our url´s clickable
