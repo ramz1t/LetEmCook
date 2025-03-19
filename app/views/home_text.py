953d0227544186412a652a1323031ca1641e174b
Roberto Rodriguez Espejo <rrodriguezesp@yahoo.es>
2025-03-19 08:00:56 +0100

    Fixed right margin

--- app/views/home_text.py
+++ app/views/home_text.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QSizePolicy
+
 
 def home_text():
     introduction = QLabel('''
@@ -11,7 +13,7 @@ def home_text():
     <b>Let'EmCook</b>
     </p>
     <br>
-    <p style="font-size: 15px;">The goal of this app is to help you manage your diet so you can keep a better track of the nutrients and calories you <br> eat to make you feel better.</p>
+    <p style="font-size: 15px;">The goal of this app is to help you manage your diet so you can keep a better track of the nutrients and calories you eat to make you feel better.</p>
 
     <hr>
     <p style="font-size: 15px;">
@@ -24,10 +26,10 @@ def home_text():
 
     <p style="font-size: 15px;">
     <b>Remember:</b><br>
-    - Having enough energy helps you stay on track with your routine. Carbohydrates offer a stable energy source while sugars may cause a <br> &nbsp; fluctuation in energy levels.<br>
+    - Having enough energy helps you stay on track with your routine. Carbohydrates offer a stable energy source while sugars may cause a fluctuation &nbsp; in energy levels.<br>
     - Foods rich in tryptophan (turkey, bananas, dairy) help produce serotonin, the "happiness hormone."<br>
-    - Stress and Anxiety Reduction: Omega-3 fatty acids (fish, nuts) and antioxidants (fruits, vegetables) can help lower stress and improve <br> &nbsp; emotional well-being.<br>
-    - Tracking your mood and your feelings with recipes will help you achieve the diet you need to confront everyday plans. Looking back at your <br> &nbsp; notes can give you valuable insights to improve your diet and well-being.<br>
+    - Stress and Anxiety Reduction: Omega-3 fatty acids (fish, nuts) and antioxidants (fruits, vegetables) can help lower stress and improve &nbsp; emotional well-being.<br>
+    - Tracking your mood and your feelings with recipes will help you achieve the diet you need to confront everyday plans. Looking back at &nbsp; your notes can give you valuable insights to improve your diet and well-being.<br>
     </p>
     <hr>
     <p style="font-size: 15px;"><b>This app has been created by:</b>
@@ -43,7 +45,11 @@ def home_text():
     </div>
     ''')
 
-    # Hace los links clickeables
+    # Makes url's clickable
     introduction.setOpenExternalLinks(True)
+    # Cuts the lines when they reach the edge
+    introduction.setWordWrap(True)
+    # Forces the text to extend to the specified width, otherwise will cut shorter
+    introduction.setFixedWidth(930)
 
     return introduction
